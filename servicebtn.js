3493d323cebeeb0db575d5195748c0e5682263a5
petarpopovic914 <134959981+petarpopovic914@users.noreply.github.com>
2024-10-10 00:34:09 +0200

    Update servicebtn.js

--- servicebtn.js
+++ servicebtn.js
@@ -1,2 +1,7 @@
-document.getElementById('exploreBtn').addEventListener('click', function() {
-        document.getElementById('services').scrollIntoView({ behavior: 'smooth' });
+document.getElementById('exploreBtn').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent any default behavior
+        document.getElementById('services').scrollIntoView({ 
+            behavior: 'smooth',
+            block: 'start'
+        });
+});
